2f61d0bcd9afc453b224f9238a804c95431821f5
Ahmed Abouzied <ahmed.abouzied@adjust.com>
2023-12-21 18:33:57 +0100

    Decode and grep each file in a task
    Signed-off-by: Ahmed Abouzied <ahmed.abouzied@adjust.com>

---
+++ src/main.rs
@@ -0,0 +1,117 @@
+use std::fs::File;
+use std::env;
+use std::process;
+
+
+use grep_regex::RegexMatcher;
+use grep_searcher::Searcher;
+use grep_searcher::sinks::UTF8;
+
+use anyhow::Result;
+use futures::future::join_all;
+
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Collect file paths from command line arguments
+    let args: Vec<String> = env::args().collect();
+
+    // Check that we have at least one file path
+    if args.len() < 2 {
+        eprintln!("Usage: zstd_gzip <regex> <file1> <file2> ...");
+        process::exit(1);
+    }
+
+    let regex = &args[1];
+    let files = &args[2..];
+
+    // handles is a vector of futures that will be executed concurrently
+    let mut handles = Vec::new();
+    for file_path in files {
+        let regex = regex.clone(); // Clone regex for each task
+        let file_path = file_path.clone(); // Clone file_path for each
+                                           
+        // Spawn a task to process for the file
+        let handle = tokio::spawn(async move {
+            match process_file(&file_path, &regex).await {
+                Ok(_) => (),
+                Err(e) => {
+                    eprintln!("Error processing file {}: {}", file_path, e);
+                    process::exit(1);
+                }
+            }
+        });
+        // Add the task to the vector of tasks
+        handles.push(handle);
+    }
+
+    // Join all the tasks and wait for them all to complete
+    let _ = join_all(handles).await;
+
+
+    Ok(())
+}
+
+/// Processes a single file. 
+/// It will stream the file into a decoder and stream the 
+/// decoded data into a searcher. The searcher will then
+/// perform a regext "grep" and print the results to stdout.
+async fn process_file(file_path: &str, regex: &str) -> Result<()> {
+    let file = match File::open(file_path){
+        Ok(file) => file,
+        Err(e) => {
+            let e = anyhow::anyhow!("Error opening file {}: {}", file_path, e);
+            return Err(e.into());
+        }
+    
+    };
+
+    if file.metadata()?.len() == 0 {
+        // File is empty, nothing to do
+        return Ok(());
+    }
+
+    if file.metadata()?.file_type().is_dir() {
+        // File is a directory, nothing to do
+        return Err(anyhow::anyhow!("{} is a directory", file_path));
+    }
+
+    if file.metadata()?.file_type().is_symlink() {
+        // File is a symlink, nothing to do
+        // we don't follow symlinks
+        return Err(anyhow::anyhow!("{} is a symlink", file_path));
+    }
+
+    // Read zstd encoded data from stdin and decode
+    let decoder = match zstd::stream::read::Decoder::new(file){
+        Ok(decoder) => decoder,
+        Err(e) => {
+            let e = anyhow::anyhow!("Error creating decoder for file {}: {}", file_path, e);
+            return Err(e.into());
+        }
+    };
+
+    let matcher = match RegexMatcher::new(&regex){
+        Ok(matcher) => matcher,
+        Err(e) => {
+            let e = anyhow::anyhow!("Error compiling regex {}: {}", regex, e);
+            return Err(e.into());
+        }
+    };
+
+    match Searcher::new().search_reader(&matcher, decoder, UTF8(|_lnum, line| {
+        // Print the line to stdout
+        // Here we use print!() instead of println!() because
+        // each line already has a newline character at the end.
+        print!("{}", line);
+        Ok(true) // Return true in the lambda function to continue searching
+    })){
+        Ok(_) => (),
+        Err(e) => {
+            let e = anyhow::anyhow!("Error searching file {}: {}", file_path, e);
+            return Err(e.into());
+        }
+    };
+
+    Ok(())
+}
